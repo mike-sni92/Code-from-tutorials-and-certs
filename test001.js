c11800234d273dcf25a8b3429a26c6a258aa1c31
mike-sni92 <117686392+mike-sni92@users.noreply.github.com>
2022-12-03 02:14:47 +0800

    Update test001.js

--- test001.js
+++ test001.js
@@ -1,1 +1,6 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
 const wordBlanks ="this noun " + myNoun + "followed by my verb " + myVerb + "then my adjective" + myAdjective + "and my adverb " + myAdverb + "." ;
