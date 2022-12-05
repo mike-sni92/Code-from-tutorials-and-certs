b2676e930b4f3a33af18fad2dfe4e9d2b0858f8c
mike-sni92 <117686392+mike-sni92@users.noreply.github.com>
2022-12-06 06:10:13 +0800

    Update test001_py_neet.py

--- test001_py_neet.py
+++ test001_py_neet.py
@@ -5,8 +5,12 @@ print('n =', n)
 n = "abc"
 print('n =', n)
 
-#type is determined at run time
-#n when printed out goes from 0 and changes to "abc
+  #type is determined at run time
+  #n when printed out goes from 0 and changes to "abc
 
 
 ##multiple assignments
+##none is null
+###if statements don't need parentheses or curly braces
+  ##indentation is used
+##Parentheses needed  for multi-line condition.
