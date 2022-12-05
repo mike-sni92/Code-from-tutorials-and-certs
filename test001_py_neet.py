54787e009de684d25b9da2aa8237079e275b3876
mike-sni92 <117686392+mike-sni92@users.noreply.github.com>
2022-12-06 06:04:33 +0800

    Create test001_py_neet.py

---
+++ test001_py_neet.py
@@ -0,0 +1,12 @@
+#Variables are dynamically typed.
+n = 0
+print('n =', n)
+
+n = "abc"
+print('n =', n)
+
+#type is determined at run time
+#n when printed out goes from 0 and changes to "abc
+
+
+##multiple assignments
