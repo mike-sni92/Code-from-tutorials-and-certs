6b48365388d92bb79388a7259667b03ee5753368
mike-sni92 <117686392+mike-sni92@users.noreply.github.com>
2022-12-10 03:33:06 +0800

    Update test001.js

--- test001.js
+++ test001.js
@@ -33,11 +33,6 @@ const removedFromMyArray = myArray.shift(1);
   var processed = 0;
 
 function processArg(num) {
-  return (num + 3) / 5;
-}
-
-processed = processArg(7);
-  //processed should be equal to 2.
   
   //*******************************************************
     
@@ -150,4 +145,38 @@ Passed:testSize(25)
 
 // Change these values to test
 golfScore(5, 1);
+  
+
+  //*******************************************************
+    
+  function caseInSwitch(val) {
+  let answer = "";
+    
+  switch (val) {
+  case 1:
+  answer = "alpha";
+  break;
+  case 2:
+  answer = "beta";
+  break;
+  case 3:
+  answer = "gamma";
+  break;
+  case 4:
+  answer = "delta";
+  break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
+  //processed should be equal to 2.
+  
+  //*******************************************************
   
