f262ddfda5b4b34be9466c181315b7be2424d4b4
mike-sni92 <117686392+mike-sni92@users.noreply.github.com>
2023-08-05 03:47:42 +0800

    Added Google Cyber Sec notes

--- test001_py_neet.py
+++ test001_py_neet.py
@@ -26,3 +26,83 @@ elif OS == "OS_1":
  print("system update badly needed")
 else:
  print("system updated")
+
+
+computer_assets = ["laptop1", "desktop20", "smartphone03"]
+for asset in computer_assets:
+    print(asset)
+  
+computer_assets = ["laptop1", "desktop20", "smartphone03"]
+print(computer_assets)
+  
+#output computer_assests = ["laptop1", "desktop20", "smartphone03"] 
+# output asset = 
+#laptop1
+#desktop20
+#smartphone03
+
+string = "security"
+for character in string:
+    print(character)
+#works on strings too, output each letter per line.
+
+#RANGE for loops
+for i in range(0, 5, 1):
+    print(i)
+    #output = 0 as starting point, 5 as end point, 1 as increment /// ""but if the start point is the default value of 0 and the increment is the default value of 1""
+
+
+#As long as the condition is True, the loop continues, but when it evaluates to False, the while loop exits. The following while loop continues as long as the condition that i < 5 is True:
+i = 1
+while i < 5:
+    print(i)
+    i = i + 1
+
+#integers in loop condition
+login_attempts = 0
+while login_attempts < 5:
+    print("Login attempts:", login_attempts)
+    login_attempts = login_attempts + 1
+
+#Booleans in loop condition
+count = 0
+login_status = True
+while login_status == True:
+    print("Try again.")
+    count = count + 1
+    if count == 4:
+        login_status = False
+
+#BREAKS
+computer_assets = ["laptop1", "desktop22", "smartphone03", "desktop0", "desktop20"]
+for asset in computer_assets:
+    if asset == "desktop20":
+        break
+    print(computer_assets)
+    #output = entire computer_assets string displayed until it reaches "desktop20"
+
+#continue
+computer_assets = ["laptop1", "desktop20", "smartphone03"]
+for asset in computer_assets:
+    if asset == "desktop20":
+        continue
+    print(asset)
+    #output, asset will not include desktop20
+
+#EXERCISE----You want to welcome 3 users from a list by their name (for example, “Welcome, Emerick Larson”).
+
+name = ["El Lar", "Ken Lma", "Cam ros"]
+for i in name:
+  print("Welcome,", i)
+  #output Welcome, El Lar and so on.
+
+name = ["El Lar", "Ken Lma", "Cam ros"]
+for i in name:
+  print("Welcome,", name)
+  #output = ["El Lar", "Ken Lma", "Cam ros"] 3X
+
+name = ["El Lar", "Ken Lma", "Cam ros"]
+for i in range(3):
+  print("Welcome,", i)
+  #output = Welcome, 0 Welcome, 1 Welcome, 2
+
